5a43b723b24efbf54960ac33c5e8fcbbe1af700b
Vivien De saint-exupery <vde-sain@z3r7p2.le-101.fr>
2019-06-21 13:48:07 +0200

    ex02 finished

--- GITLAB_DOSS/day09/ex02/todo.js
+++ GITLAB_DOSS/day09/ex02/todo.js
@@ -3,27 +3,54 @@ function PromptMessage()
 	var entry = prompt("new to-do: ", "");
 	if (entry == "" || entry == null)
 		return;
-	console.log("entry="+entry);
-	var new_div = document.createElement("div");
-	new_div.setAttribute("onclick", "delete_node(this, entry)");
-	new_div.setAttribute("class", "baby_div");
-	new_div.appendChild(document.createTextNode(entry));
 	var mother = document.getElementById("ft_list");
-	mother.insertBefore(new_div, mother.childNodes[0]);
 	var nb_nodes = mother.childElementCount - 1;
+	var new_div = document.createElement("div");
+	new_div.setAttribute("onclick", "delete_node(this.id, this)");
+	new_div.setAttribute("id", "child"+nb_nodes);
+	new_div.appendChild(document.createTextNode(entry));
+	mother.insertBefore(new_div, mother.childNodes[0]);
 	console.log("nb_nodes = "+ nb_nodes);
-	document.cookie = "cookie["+nb_nodes+"]="+entry;
+	document.cookie = "child"+nb_nodes+"="+entry;
 	var cook = document.cookie;
-
 }
 
-function delete_node(name, entry)
+function delete_node(id, object)
 {
-	console.log(name);
-	console.log(entry);
-	name.parentNode.removeChild(name);
-	array = name.split(">");
-	print(array);
-	document.cookie = name+"; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+	if (!confirm("Are you sure you want to delete this task ?"))
+		return;
+	console.log("ICI="+id);
+	document.cookie = id + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+	object.parentNode.removeChild(object);
 	console.log("deleting node");
 }
+
+function Onload_cookie(cook)
+{
+	var mother = document.getElementById("ft_list");
+	var nb_nodes = mother.childElementCount - 1;
+	var new_div = document.createElement("div");
+	new_div.setAttribute("onclick", "delete_node(this.id, this)");
+	new_div.setAttribute("id", "child"+nb_nodes);
+	var value = cook.split("=");
+	new_div.appendChild(document.createTextNode(value[1]));
+	mother.insertBefore(new_div, mother.childNodes[0]);
+	console.log("nb_nodes = "+ nb_nodes);
+	document.cookie = "child"+nb_nodes+"="+value[1];
+	var cook = document.cookie;
+}
+
+function getCookies()
+{
+	var cook = document.cookie.split(";");
+	console.log(cook.length);
+	var i = 0;
+	if (cook == "")
+		return;
+	for (i = 0; i < cook.length; i++)
+	{
+		console.log(cook[i]);
+		Onload_cookie(cook[i]);
+	}
+}
+
