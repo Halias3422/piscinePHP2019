368f327429490c176bc1af9668eb0554fc476c16
Vivien De saint-exupery <vde-sain@z3r7p2.le-101.fr>
2019-06-21 12:37:50 +0200

    ex02

--- GITLAB_DOSS/day09/ex02/todo.js
+++ GITLAB_DOSS/day09/ex02/todo.js
@@ -1,21 +1,29 @@
 function PromptMessage()
 {
 	var entry = prompt("new to-do: ", "");
-	if (entry == "")
+	if (entry == "" || entry == null)
 		return;
-	console.log(entry);
+	console.log("entry="+entry);
 	var new_div = document.createElement("div");
-	new_div.setAttribute("onclick", "delete_node(this)");
+	new_div.setAttribute("onclick", "delete_node(this, entry)");
+	new_div.setAttribute("class", "baby_div");
 	new_div.appendChild(document.createTextNode(entry));
 	var mother = document.getElementById("ft_list");
 	mother.insertBefore(new_div, mother.childNodes[0]);
-	document.cookie = 'cookie1=' + entry + '; expires=Fri, 21 Feb 2020 00:00:00 UTC;';
-	console.log(document.cookie);
+	var nb_nodes = mother.childElementCount - 1;
+	console.log("nb_nodes = "+ nb_nodes);
+	document.cookie = "cookie["+nb_nodes+"]="+entry;
+	var cook = document.cookie;
+
 }
 
-function delete_node(name)
+function delete_node(name, entry)
 {
 	console.log(name);
+	console.log(entry);
 	name.parentNode.removeChild(name);
-console.log("deleting node");
+	array = name.split(">");
+	print(array);
+	document.cookie = name+"; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+	console.log("deleting node");
 }
